d0184f56243321e28108cda059cfcd87feda4db2
Krista Cannady <kristalcannady@gmail.com>
2022-10-12 15:36:05 -0400

    route files set up

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3001;
 // initiates the server/invokes express
 const app = express();
 
+//constant for the routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// allows express to handle data as JSON
+app.use(express.urlencoded({ extended: true }));
+// allows us to interact with data in JSON format
+app.use(express.json());
+// any stuff that is static, use the public folder
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // Makes the server listen to us - keep at the end
 app.listen(3001, () => {
