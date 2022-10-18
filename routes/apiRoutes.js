82599944b2ab45ad8ae358426ba42aa0c42a2437
Krista Cannady <kristalcannady@gmail.com>
2022-10-18 19:58:57 -0400

    post attempt

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,32 @@
-// set up 
-const path = require('path');
 const router = require('express').Router();
+const { notes } = require('../db/db.json');
 const fs = require('fs');
-// notes format set up 
-const Notes = require('../db/db.json');
-const { networkInterfaces } = require('os');
+const path = require('path');
 
+// This is the function to add notes
+function createNewNote(body, dbArray) {
+  const notes = body;
+  dbArray.push(notes);
+  // write the file 
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notes: dbArray }, null, 2)
+  )
+  return notes;
+}
 
+// get current notes 
+router.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
 
-// part of set up
+// post reqest to send notes 
+router.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes)
+  res.json(note);
+  
+})
+
 module.exports = router;
